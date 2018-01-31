6eb2359c0caa1fa33b3311df1843aa3dd47439eb
arzelachen <arzelachen@gmail.com>
2018-01-31 15:28:20 -0800

    optimize network speed

--- Agent.py
+++ Agent.py
@@ -7,15 +7,17 @@ from torch.autograd import Variable
 import inspect
 from torch.nn.utils import rnn
 from utils_pad import pack_sequence
-from utils_pad import pad_sequence
+# from utils_pad import pad_sequence
 from utils_pad import prepare_sequence
+# from torch.nn.utils.rnn import pack_sequence
 
 FloatTensor = torch.cuda.FloatTensor
 LongTensor = torch.cuda.LongTensor
 ByteTensor = torch.cuda.ByteTensor
 
+
 class DQRN(nn.Module):
-    def __init__(self, input_size, hidden_size_low, hidden_size_high):
+    def __init__(self, n_sample, input_size, hidden_size_low, hidden_size_high):
         super(DQRN, self).__init__()
         self.hidden_size_low = hidden_size_low
         self.hidden_size_high = hidden_size_high
@@ -23,14 +25,25 @@ class DQRN(nn.Module):
         self.gru_low = nn.GRU(input_size, hidden_size_low, batch_first=False, bidirectional=False)
         self.gru_high = nn.GRU(hidden_size_low, hidden_size_high, batch_first=True, bidirectional=False)
 
-        self.state_fc = nn.Linear(hidden_size_high, 16)
-        self.cluster_fc = nn.Linear(hidden_size_low, 16)
-        self.agent_fc1 = nn.Linear(32, 32)
+        self.state_fc = nn.Linear(hidden_size_high, 32)
+        self.cluster_fc = nn.Linear(hidden_size_low, 32)
+        self.agent_fc1 = nn.Linear(64, 32)
         self.agent_fc2 = nn.Linear(32, 1)
+
+        n_action = n_sample*(n_sample-1)/2
+        self.row_idx = LongTensor([0]*n_action)
+        self.col_idx = LongTensor([0]*n_action)
+        count = 0
+        for i in range(n_sample):
+            for j in range(i):
+                self.row_idx[count] = i
+                self.col_idx[count] = j
+                count += 1
 
     def init_hidden(self):
         return Variable(torch.zeros(1,1,self.hidden_size_low).type(FloatTensor)), Variable(torch.zeros(1,1,self.hidden_size_high).type(FloatTensor))
 
+    # @profile
     def forward(self, input):
         partition, images = input
         n_images = images.size(0)
@@ -46,26 +59,36 @@ class DQRN(nn.Module):
         # print cluster_rep
         state_rep = torch.squeeze(state_rep)
 
-        cluster_rep = F.relu(self.cluster_fc(cluster_rep))
         state_rep = F.relu(self.state_fc(state_rep))
-        q_table = Variable(torch.zeros(n_cluster*(n_cluster-1)/2).type(FloatTensor))
+        cluster_rep = F.relu(self.cluster_fc(cluster_rep))
 
-        count = 0
-        for i in range(n_cluster):
-            for j in range(i):
-                merge_cluster = cluster_rep[i,:] + cluster_rep[j,:]
-                merge_rep = torch.cat([state_rep, merge_cluster])
-                q = F.relu(self.agent_fc1(merge_rep))
-                q = self.agent_fc2(q)
-                q_table[count] = q 
+        # q_table = Variable(torch.zeros(n_cluster*(n_cluster-1)/2).type(FloatTensor))
+        # count = 0
+        # for i in range(n_cluster):
+        #     for j in range(i):
+        #         merge_cluster = cluster_rep[i,:] + cluster_rep[j,:]
+        #         merge_rep = torch.cat([state_rep, merge_cluster])
+        #         q = F.relu(self.agent_fc1(merge_rep))
+        #         q = self.agent_fc2(q)
+        #         q_table[count] = q
+        #         count += 1
+        # q_table = nn.Softmax()(q_table)
 
-                count += 1
+        n_action = n_cluster*(n_cluster-1)/2
+        row_idx = self.row_idx[:n_action]
+        col_idx = self.col_idx[:n_action]
+        merge_cluster = cluster_rep[row_idx,:]+cluster_rep[col_idx,:]
+        tile_state = state_rep.repeat(n_action,1)
+        merge_rep = torch.cat([tile_state, merge_cluster],dim=1)
+        q_table = F.relu(self.agent_fc1(merge_rep))
+        q_table = self.agent_fc2(q_table)
+        q_table = nn.Softmax(dim=0)(q_table)
 
-        q_table = nn.Softmax()(q_table)
         return q_table
 
+
 class CONV_DQRN(nn.Module):
-    def __init__(self, hidden_fc, hidden_size_low, hidden_size_high):
+    def __init__(self, n_sample, hidden_fc, hidden_size_low, hidden_size_high):
         super(CONV_DQRN, self).__init__()
         self.conv1 = nn.Conv2d(1,32,kernel_size=5)
         self.conv2 = nn.Conv2d(32,64,kernel_size=5)
@@ -81,6 +104,16 @@ class CONV_DQRN(nn.Module):
         self.cluster_fc = nn.Linear(hidden_size_low, 16)
         self.agent_fc1 = nn.Linear(32, 32)
         self.agent_fc2 = nn.Linear(32, 1)
+
+        n_action = n_sample*(n_sample-1)/2
+        self.row_idx = LongTensor([0]*n_action)
+        self.col_idx = LongTensor([0]*n_action)
+        count = 0
+        for i in range(n_sample):
+            for j in range(i):
+                self.row_idx[count] = i
+                self.col_idx[count] = j
+                count += 1
 
     def init_hidden(self):
         return Variable(torch.zeros(1,1,self.hidden_size_low).type(FloatTensor)), Variable(torch.zeros(1,1,self.hidden_size_high).type(FloatTensor))
@@ -109,18 +142,28 @@ class CONV_DQRN(nn.Module):
 
         cluster_rep = F.relu(self.cluster_fc(cluster_rep))
         state_rep = F.relu(self.state_fc(state_rep))
-        q_table = Variable(torch.zeros(n_cluster*(n_cluster-1)/2).type(FloatTensor))
 
-        count = 0
-        for i in range(n_cluster):
-            for j in range(i):
-                merge_cluster = cluster_rep[i,:] + cluster_rep[j,:]
-                merge_rep = torch.cat([state_rep, merge_cluster])
-                q = F.relu(self.agent_fc1(merge_rep))
-                q = self.agent_fc2(q)
-                q_table[count] = q 
+        # q_table = Variable(torch.zeros(n_cluster*(n_cluster-1)/2).type(FloatTensor))
+        # count = 0
+        # for i in range(n_cluster):
+        #     for j in range(i):
+        #         merge_cluster = cluster_rep[i,:] + cluster_rep[j,:]
+        #         merge_rep = torch.cat([state_rep, merge_cluster])
+        #         q = F.relu(self.agent_fc1(merge_rep))
+        #         q = self.agent_fc2(q)
+        #         q_table[count] = q
+        #
+        #         count += 1
+        # q_table = nn.Softmax()(q_table)
 
-                count += 1
+        n_action = n_cluster*(n_cluster-1)/2
+        row_idx = self.row_idx[:n_action]
+        col_idx = self.col_idx[:n_action]
+        merge_cluster = cluster_rep[row_idx,:]+cluster_rep[col_idx,:]
+        tile_state = state_rep.repeat(n_action,1)
+        merge_rep = torch.cat([tile_state, merge_cluster],dim=1)
+        q_table = F.relu(self.agent_fc1(merge_rep))
+        q_table = self.agent_fc2(q_table)
+        q_table = nn.Softmax(dim=0)(q_table)
 
-        q_table = nn.Softmax()(q_table)
         return q_table
--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import math
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F 
+import torch.nn.functional as F
 from torch.autograd import Variable
 from utils_pad import prepare_sequence
 from Agent import DQRN
@@ -14,51 +14,60 @@ from Agent import CONV_DQRN
 from env import env
 from itertools import count
 
+
 FloatTensor = torch.cuda.FloatTensor
 LongTensor = torch.cuda.LongTensor
 ByteTensor = torch.cuda.ByteTensor
 
-def pair_from_index(index):
-    i = int((2*index+0.25)**0.5+0.5)
-    j = index - i*(i-1)/2
 
-    return env.Action(i,j)
+def pair_from_index(index):
+    i = int((2 * index + 0.25) ** 0.5 + 0.5)
+    j = int(index - i * (i - 1) / 2)
+
+    return env.Action(i, j)
+
 
 class ReplayMemory():
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = []
         self.position = 0
+
     def push(self, exp):
         if len(memory) < self.capacity:
             self.memory.append(None)
         self.memory[self.position] = exp
-        self.position = (self.position+1)%self.capacity
+        self.position = (self.position + 1) % self.capacity
+
     def sample(self, batch_size):
         return random.sample(self.memory, batch_size)
+
+    def is_full(self):
+        return (len(self.memory)==self.capacity)
 
     def __len__(self):
         return len(self.memory)
 
-def select_action():
+# @profile
+def select_action(train=False):
     # input = prepare_sequence(partition, images, volatile=True)
     input = [partition, images]
     n_cluster = len(partition)
-    n_action = n_cluster*(n_cluster-1)/2
+    n_action = n_cluster * (n_cluster - 1) / 2
 
     sample = random.random()
     # eps_thresh = eps_end + (eps_start-eps_end)*math.exp(-1.*steps_done/eps_decay)
-    eps_thresh = eps_end + (eps_start-eps_end)*math.exp(-1.*i_episode/eps_decay)
-    if sample > eps_thresh:
+    eps_thresh = eps_end + (eps_start - eps_end) * math.exp(-1. * i_episode / eps_decay)
+    if (not train) or sample > eps_thresh:
         action = model(input).data.max(0)[1]
     else:
         action = LongTensor([random.randrange(n_action)])
 
     return action
 
+# @profile
 def optimize():
-
-    if len(memory) < batch_size:
+    if not memory.is_full():
         return
 
     all_replay = memory.sample(batch_size)
@@ -68,11 +77,11 @@ def optimize():
 
         replay_partition = replay[0]
         replay_next_partition = replay[2]
-        
+
         replay_action = Variable(replay[1])
         replay_reward = Variable(replay[3])
         replay_images = replay[4]
-        
+
         # input = prepare_sequence(replay_partition, images)
         replay_input = [replay_partition, replay_images]
         q = model(replay_input)[replay_action]
@@ -85,80 +94,86 @@ def optimize():
             replay_next_input = [replay_next_partition, replay_images]
             result = model(replay_next_input).max(0)[0]
             # next_q = Variable(torch.zeros(1).type(FloatTensor), volatile=True)
-            next_q = Variable(result.data.type(FloatTensor), volatile=True)
+            next_q = Variable(result.data, requires_grad=False)
             # next_q[:] = result[:]
 
-            next_q.volatile = False
+            # next_q.volatile = False
             # replay_images.volatile = False
-            target_q = replay_reward + gamma*next_q
+            target_q = replay_reward + gamma * next_q
 
+        # q.register_hook(print)
 
         loss = F.smooth_l1_loss(q, target_q)
         optimizer.zero_grad()
         loss.backward()
         for param in model.parameters():
-            param.grad.data.clamp(-1,1)
+            param.grad.data.clamp(-1, 1)
         optimizer.step()
-
 
 gamma = 1
 eps_start = 0.95
 eps_end = 0.05
-eps_decay = 1000
-batch_size = 50
+# eps_start = 1
+# eps_end = 1
+eps_decay = 2000
+batch_size = 100
 
-n_episodes = 1000
+n_episodes = 5000
 data_dir = 'dataset'
-sampling_size = 10
-t_stop = 4
-clustering_env = env.Env(data_dir, sampling_size)
+sampling_size = 30
+t_stop = sampling_size-6
+clustering_env = env.Env(data_dir, sampling_size, reward='global_purity')
 
-# model = DQRN(784,32,32)
-model = CONV_DQRN(128,32,32)
+model = DQRN(sampling_size,784,32,32)
+# model = CONV_DQRN(128, 32, 32)
 model.cuda()
 
-optimizer = optim.RMSprop(model.parameters(), lr=0.01)
+optimizer = optim.RMSprop(model.parameters(), lr=0.0001)
 memory = ReplayMemory(10000)
 
 steps_done = 0
 for i_episode in range(n_episodes):
     clustering_env.set_seed(0)
-    partition, images = clustering_env.reset()
+    partition, images, _ = clustering_env.reset()
 
     random.seed(i_episode)
     partition = partition.cluster_assignments
-    images = np.concatenate(images).reshape((sampling_size,-1))
-    images = torch.from_numpy(images)
+    images = np.concatenate(images).reshape((sampling_size, -1))
+    images = torch.from_numpy(images).type(FloatTensor)
     # print images.type
 
     episode_reward = 0
     reward_list = []
+    train = True if i_episode%10!=0 else False
+
     for t in count():
-        action = select_action()
+        action = select_action(train)
 
         action_pair = pair_from_index(action[0])
-        reward, next_partition = clustering_env.step(action_pair)
+        reward, next_partition, purity = clustering_env.step(action_pair)
         next_partition = next_partition.cluster_assignments
 
         episode_reward += reward
         reward_list.append(reward)
         reward = FloatTensor([reward])
 
-        exp = [partition, action, next_partition, reward, images]
-        memory.push(exp)
+        if t == t_stop:
+            final_partition = next_partition
+            next_partition = None
+
+        if train:
+            exp = [partition, action, next_partition, reward, images]
+            memory.push(exp)
+            optimize()
+
         steps_done += 1
 
-        optimize()
-
         if t == t_stop:
-            next_partition = None
-            print('episode reward:', episode_reward)
+            if not train:
+                print('episode', i_episode, 'episode reward:', episode_reward, 'episode purity:', purity)
+                # labels = clustering_env.sampled_labels
+                # print([[labels[x] for x in cluster] for cluster in final_partition])
             # print 'reward history:', reward_list
             break
 
         partition = next_partition
-
-
-
-
-
